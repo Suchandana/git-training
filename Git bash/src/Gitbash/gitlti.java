a9fee324d82b20e6b0654d2223a1d07038ed3402
Suchandana <18211A0439@bvrit.ac.in>
2022-08-12 16:01:45 +0530

    Commit

---
+++ Git bash/src/Gitbash/gitlti.java
@@ -0,0 +1,10 @@
+package Gitbash;
+public class gitlti {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hlo everyone I am Suchandana");
+
+	}
+
+}
